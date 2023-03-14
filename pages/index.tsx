c2331e384724d676ce5a9a780b4830f42a8fa081
waynewa <52760648+waynewa@users.noreply.github.com>
2023-03-14 06:25:24 +0000

    Updated the intial pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,9 +13,26 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Main page that is being generated for the all the WWiT packages deployed 
-            </p>
+          <h1>WWiT Pages</h1>
+
+
+          <p> The purpose of this page is to showcase the various aspects of development and test automation that have been undertaken by the WWiT team. The listed repositories have been created and are currently being maintained by the WWiT developer.
+            For additional information about WWiT, please visit their website at https://wwit.netlify.app.
+          </p>
+          <h3>Test Automation projects</h3>
+          <ul>
+            <li >
+              WWiTSeleniumBase - This project is a C# based selenium framework - https://github.com/waynewa/WWiTSeleniumBase
+
+            </li>
+
+            <li>
+              WWiTSeleniumJ - This project is a Java based selenium framework - https://github.com/waynewa/WWiTSeleniumJ
+            </li>
+          </ul>
+          <h3>Development Projects</h3>
+          <ul>
+            <li >WWiT WebSite - This project is linked to the WWiT Website - https://github.com/waynewa/wwit</li></ul>
         </div>
       </main>
     </>
