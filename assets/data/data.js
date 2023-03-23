90cf564bb06b17aed257b894d7963197458d22e9
Wayne Walsh <wayne.walsh@iag.co.nz>
2023-03-23 13:31:18 +1300

    Updated projects

--- assets/data/data.js
+++ assets/data/data.js
@@ -22,9 +22,18 @@ const projects = [
     name: 'WWiT Website',
     description:
       'This webpage has been developed using React, a popular JavaScript library for building user interfaces. Its modern, component-based architecture allows for efficient development and maintenance of complex web applications. With its fast rendering and seamless data flow, this webpage provides a smooth and responsive user experience.',
-    url: 'https://github.com/waynewa/WWiT',
+    url: 'https://wwit.netlify.app',
     type: 'Web development',
     image: 'wwitscreenshot.jpg',
+  },
+  {
+    id: 4,
+    name: '2 Plus Cleaning ',
+    description:
+      'This webpage has been developed using React and Vite, a popular JavaScript library for building user interfaces. Its modern, component-based architecture allows for efficient development and maintenance of complex web applications. With its fast rendering and seamless data flow, this webpage provides a smooth and responsive user experience.',
+    url: 'https://2pluscleaning.netlify.app',
+    type: 'Web development',
+    image: '2plusscreenshot.jpg',
   },
 ];
 
